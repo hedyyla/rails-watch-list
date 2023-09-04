266737655b1ea12d15b485c77c6568d818930b72
Hedyla Yap <hedyyla@gmail.com>
2023-09-04 18:40:38 +0800

    part 4

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    @bookmark.list = List.find(params[:list_id])
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show'
+
+  resources :lists, only: [:new, :index, :create, :show] do
+    resources :bookmarks, only: [:new, :index, :create, :destroy]
+  end
+  resources :movies, only: [:index, :show]
 end
